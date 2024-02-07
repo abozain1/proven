c32c3a2557b5444752087e7940e92d8fc12a7ccc
abozain1 <ahmed1998born@gmail.com>
2024-02-07 14:52:54 +0200

    draw the canvas to full screen

--- src/comps/canvasComponent/hook.ts
+++ src/comps/canvasComponent/hook.ts
@@ -4,19 +4,30 @@ import { useEffect, useRef } from "react";
 export default function useCanvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
+  const adjustCanvasSize = () => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
       const context = canvas.getContext("2d");
-
-      if (context) {
+      if (context && canvas.width && canvas.height) {
         const image = new Image();
         image.onload = () => {
-          context.drawImage(image, 0, 0);
+          context.drawImage(image, 0, 0, canvas.width, canvas.height);
         };
         image.src = obj.base64;
       }
     }
+  };
+
+  useEffect(() => {
+    adjustCanvasSize();
+    window.addEventListener("resize", adjustCanvasSize);
+
+    return () => {
+      window.removeEventListener("resize", adjustCanvasSize);
+    };
   }, [obj]);
+
   return { canvasRef };
 }
--- src/comps/canvasComponent/index.tsx
+++ src/comps/canvasComponent/index.tsx
@@ -1,5 +1,7 @@
+import styles from "./styles.module.scss";
 import useCanvas from "./hook";
 export default function CanvasComponent() {
   const { canvasRef } = useCanvas();
+
   return <canvas ref={canvasRef} />;
 }
